4083335ed954f873617bba9f3d7d3cf420581cc8
Alex Ley <alex@alexley.co.uk>
2017-10-05 22:15:27 +0100

    untested k8s demo script

--- scripts/0-check-k8s-environment.sh
+++ scripts/0-check-k8s-environment.sh
@@ -2,6 +2,17 @@
 
 echo "Checking k8s environment is clean and ready..."
 
+#TODO check if minikube is running
+cd $HOME/workspace/service-catalog
+minikube start
+
+#TODO check if helm & service catalog is setup
+#DEPLOY the service-catalog
+#Use helm to setup the components
+helm init
+helm install charts/catalog --name catalog --namespace catalog
+
+#FIND service catalog api endpoint 
 SVC_CAT_API=$(minikube service -n catalog catalog-catalog-apiserver --url | sed -n 1p)
 echo $SVC_CAT_API
 
--- scripts/3-k8s.sh
+++ scripts/3-k8s.sh
@@ -5,8 +5,61 @@
 ########################
 . scripts/resources/demo-magic.sh
 
+#GET the address of the controller
+SVC_CAT_API=$(minikube service -n catalog catalog-catalog-apiserver --url | sed -n 1p)
+echo $SVC_CAT_API
+
+#CREATE a new kubectl context
+kubectl config set-cluster service-catalog --server=$SVC_CAT_API
+kubectl config set-context service-catalog --cluster=service-catalog
+
 # hide the evidence
 clear
 
 # put your stuff here
-pe 
+#SEE no broker has yet been registered
+pe "kubectl --context=service-catalog get brokers,serviceclasses,instances,bindings"
+
+#REGISTER a broker - we will use the earlier deployed regis
+pe "cat resources/overview-broker.yaml"
+pe "kubectl --context=service-catalog create -f resources/overview-broker.yaml"
+
+#GET the service class for the resources/overview-broker service
+#This shows details of the service and plans available
+pe "kubectl --context=service-catalog get serviceclasses"
+pe "kubectl --context=service-catalog get serviceclass resources/overview-broker -o yaml"
+
+#CREATE a namespace for development
+#All of our service instances and bindings will live here
+pe "kubectl create ns development"
+
+# #CREATE a service instance on the default plan
+pe "cat resources/overview-instance.yaml"
+pe "kubectl --context=service-catalog create -f resources/overview-instance.yaml"
+
+#GET the service instance
+pe "kubectl --context=service-catalog -n development get instances"
+pe "kubectl --context=service-catalog -n development get instances -o yaml"
+
+#CREATE a binding to the instance
+pe "cat resources/overview-binding.yaml"
+pe "kubectl --context=service-catalog create -f resources/overview-binding.yaml"
+
+#GET the service binding
+pe "kubectl --context=service-catalog -n development get binding"
+pe "kubectl --context=service-catalog -n development get binding  -o yaml"
+
+#GET the secret
+pe "kubectl get secrets -n development"
+pe "kubectl get secrets -n development resources/overview-credentials -o yaml"
+
+#DELETE the binding
+pe "kubectl --context=service-catalog -n development get binding"
+pe "kubectl --context=service-catalog -n development delete binding resources/overview-binding"
+
+#SEE binding and secret has gone
+pe "kubectl --context=service-catalog -n development get binding"
+pe "kubectl get secrets -n development"
+
+#DELETE the instance
+pe "kubectl --context=service-catalog -n development delete binding resources/overview-binding"
