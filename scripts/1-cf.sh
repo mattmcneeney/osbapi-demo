f97426b004737545fed50b4a021ec8b0c664c566
Matt McNeeney <mmcneeney@pivotal.io>
2017-10-09 17:31:41 +0100

    dont push basic app

--- scripts/1-cf.sh
+++ scripts/1-cf.sh
@@ -16,6 +16,7 @@ clear
 
 # put your stuff here
 BROKER_URL=http://$(cf app overview-broker-cf-summit | awk '/urls:/{ print $2 }')
+pe "cf apps"
 pe "open '${BROKER_URL}'"
 pe "cf marketplace"
 pe "cf create-service-broker overview-broker-cf-summit admin password ${BROKER_URL}"
@@ -23,9 +24,7 @@ pe "cf enable-service-access overview-broker-cf-summit"
 pe "cf marketplace"
 pe "cf create-service overview-broker-cf-summit simple my-service"
 pe "cf services"
-p "git clone git@github.com:mattmcneeney/extremely-basic-node-app.git"
-pe "cd extremely-basic-node-app"
-pe "cf push"
+pe "cf apps"
 pe "cf bind-service extremely-basic-node-app my-service"
 pe "cf restart extremely-basic-node-app"
 pe "cf env extremely-basic-node-app"
