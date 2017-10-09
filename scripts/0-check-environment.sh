32316a82641341b02f6e60fa2b6c7f1d20c16b0f
Matt McNeeney <mmcneeney@pivotal.io>
2017-10-09 16:02:14 +0100

    do not delete app

--- scripts/0-check-environment.sh
+++ scripts/0-check-environment.sh
@@ -2,9 +2,9 @@
 
 echo "Checking environment is clean and ready..."
 
-cf apps | grep "overview-broker-cf-summit" > /dev/null &&
+cf apps | grep "overview-broker-cf-summit" > /dev/null ||
 (
-   echo "WARNING: overview-broker-cf-summit app exists" &&
+   echo "WARNING: overview-broker-cf-summit app does not exist" &&
    exit 1
 )
 
--- scripts/0-cleanup.sh
+++ scripts/0-cleanup.sh
@@ -2,7 +2,6 @@
 
 cf delete-service -f my-service
 cf delete-service-broker -f overview-broker-cf-summit
-cf delete -f overview-broker-cf-summit
 cf delete -f extremely-basic-node-app
 rm -r /tmp/demo
 
