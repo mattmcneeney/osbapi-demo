8a487bb2456b76ce24e1bf4c60035ee953e318ec
Matt McNeeney <mmcneeney@pivotal.io>
2017-11-21 10:54:34 +0000

    fix check env script

--- scripts/0-check-environment.sh
+++ scripts/0-check-environment.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -e
 
-declare -a env_vars=("SERVICE_BROKER_APP_NAME SERVICE_BROKER_NAME SERVICE_INSTANCE_NAME SERVICE_BINDING_NAME CREDENTIALS_NAME")
+declare -a env_vars=("SERVICE_BROKER_APP_NAME" "SERVICE_BROKER_NAME" "SERVICE_INSTANCE_NAME" "SERVICE_BINDING_NAME" "CREDENTIALS_NAME")
 
 for v in "${env_vars[@]}"
 do
-   if [ -z "$${v}" ]; then
+   if [ -z ${!v} ]; then
       echo "ERROR: Missing environmental variable $v"
-      exit 1
+   else
+      echo "$v: ${!v}"
    fi
 done
 
