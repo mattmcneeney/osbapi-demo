b31ffb9fb0bcfb80ad398f53ca2bce208722bbaa
Matt McNeeney <mmcneeney@pivotal.io>
2017-11-22 10:22:06 +0000

    tidy up k8s setup

--- scripts/k8s/setup-env.sh
+++ scripts/k8s/setup-env.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -v
+#!/bin/bash -ex
 
 minikube start --extra-config=apiserver.Authorization.Mode=RBAC
 
@@ -9,12 +9,9 @@ kubectl create clusterrolebinding tiller-cluster-admin \
     --clusterrole=cluster-admin \
     --serviceaccount=kube-system:default
 
+# The helm install will fail if we run this immediately. Give tiller
+# some time to get ready...
+sleep 30
 helm install svc-cat/catalog \
     --name catalog --namespace catalog --set insecure=true
-
-SVC_CAT_API=$(minikube service -n catalog catalog-catalog-apiserver --url | sed -n 1p)
-echo $SVC_CAT_API
-
-kubectl config set-cluster service-catalog --server=$SVC_CAT_API
-kubectl config set-context service-catalog --cluster=service-catalog
 
